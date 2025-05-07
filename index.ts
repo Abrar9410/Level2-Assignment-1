6c36f2a166d4ea5c61e26961ae33536092ac1569
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-05-07 23:25:20 +0600

    Problem-3 done

--- index.ts
+++ index.ts
@@ -13,3 +13,11 @@ function filterByRating(items: Item[]): Item[] {
     const filteredItems: Item[] = items.filter((item: Item) => item.rating >= 4);
     return filteredItems;
 };
+
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    const concatenatedArray: T[] = [];
+    arrays.map((array: T[]) => concatenatedArray.push(...array));
+    return concatenatedArray;
+}
